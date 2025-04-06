d5acb0786cc7fab32b697242e3f073748802afc4
Jesper Persson <Jesperpersson96@hotmail.se>
2025-04-06 23:18:49 +0200

    working on home.jsx/css

--- src/components/menuBtn.jsx
+++ src/components/menuBtn.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
-
-function MenuBtn() {
-  const tooglemenu = () =>{
-    
-  }
-  return (
-    <img src="src/assets/menu.png" height={"40px"}  width={"40px"} alt="menu button" />
+const Button = ({children, variant, onClick}) =>{
+  return(
+    <button onClick={onClick} className={`btn-${variant}`}>{children}</button>
   )
 }
 
-export default MenuBtn
+export default Button
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import "../sassFiles/Home/Home.scss";
-import MenuBtn from "../components/menuBtn";
 import { Link } from "react-router-dom";
+import Button from "../components/menuBtn";
 
 export default function Home() {
   return (
     <>
       <section className="home-container">
-        <header>
-          <div className="header-container">
+        <header className="header-container">
             <span className="home-motto">Family,Code,Roll,Run,Repeat</span>
-            <MenuBtn />
-          </div>
+              <Button variant={"menu"} ><img  className="btn-menu" src="src/assets/menu.png" alt="menu button" /></Button>
         </header>
 
-        <main>
+        <main className="main-container">
           <h2 className=" home-title">Hey!</h2>
           <span className=" about-me">
             I’m Jesper, 28-year-old aspiring JavaScript developer based in
@@ -25,11 +23,13 @@ export default function Home() {
             apps—and I’m always hungry to learn more.
           </span>
           <div className="side-line"></div>
-          <Link to={"/portfolio"}>
-            <button>Portfolie</button>
+          <Link className="portfolie-btn" to={"/portfolio"}>
+            <button className="portfolie-btn">Portfolie</button>
           </Link>
         </main>
         <footer className="footer-container">
+          <div className="social-box">
+
           <img
             className="footer-social"
             onClick={() =>
@@ -54,13 +54,14 @@ export default function Home() {
             }
             src="/src/assets/linkedin.png"
             alt="likedin icon"
-          />
+            />
           <img
             className="footer-social"
             onClick={() => (window.location.href = "tel:+0701471434")}
             src="/src/assets/telephone.png"
             alt="telephone icon"
           />
+            </div>
           <span className="footer-text">
             “Fullstack. focused. Curious. Consistent.”
           </span>
